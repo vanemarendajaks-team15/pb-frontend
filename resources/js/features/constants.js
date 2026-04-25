48b201c71ebbcf0a65d3b10348573d2e86e39866
Margus Käen <margus.kaen@gmail.com>
2026-04-26 00:54:16 +0300

    Registration view modifications. Needs database changes probally

--- resources/js/features/constants.js
+++ resources/js/features/constants.js
@@ -3,3 +3,5 @@ export const TOURNAMENT_STATUS_TYPES = {
     LIVE: 'live',
     FUTURE: 'future'
     }
+
+export const EMAIL_VALIDATION_PATTERN = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
--- resources/js/pages/auth/Register.vue
+++ resources/js/pages/auth/Register.vue
@@ -1,8 +1,13 @@
 <script setup>
 import { Head, Link, useForm } from '@inertiajs/vue3';
+import AuthButton from '../../components/buttons/AuthButton.vue';
+import TheHeader from '../../components/layout/TheHeader.vue';
+import BaseCard from '../../components/ui/BaseCard.vue';
 
 const form = useForm({
-    name: '',
+    firstName: '',
+    lastName: '',
+    sex: 'M',
     email: '',
     password: '',
     password_confirmation: '',
@@ -19,81 +24,111 @@ function submit() {
 
 <template>
     <Head title="Register" />
-
+    <TheHeader />
     <div class="page">
-        <div class="stack stack--lg">
-            <h1>Register</h1>
+        <BaseCard>
+            <div class="stack stack--lg">
+                <h1>Register</h1>
 
-            <form class="stack" @submit.prevent="submit">
-                <div class="field">
-                    <label for="name">Name</label>
-                    <input
-                        id="name"
-                        v-model="form.name"
-                        type="text"
-                        name="name"
-                        required
-                        autocomplete="name"
+                <form class="stack" @submit.prevent="submit">
+                    <div class="field">
+                        <label for="firstName">First name</label>
+                        <input
+                            id="firstName"
+                            v-model.trim="form.firstName"
+                            type="text"
+                            name="firstName"
+                            required
+                            autocomplete="given-name"
+                        />
+                        <span v-if="form.errors.firstName" class="error">{{
+                            form.errors.firstName
+                        }}</span>
+                    </div>
+
+                    <div class="field">
+                        <label for="lastName">Last Name</label>
+                        <input
+                            id="lastName"
+                            v-model.trim="form.lastName"
+                            type="text"
+                            name="lastName"
+                            required
+                            autocomplete="family-name"
+                        />
+                        <span v-if="form.errors.lastName" class="error">{{
+                            form.errors.lastName
+                        }}</span>
+                    </div>
+
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="Male" id="Male" v-model="form.sex" value="M" checked>
+                        <label class="form-check-label" for="Male">
+                            Male
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="Female" id="Female" v-model="form.sex" value="F">
+                        <label class="form-check-label" for="Female">
+                            Female
+                        </label>
+                    </div>
+
+
+                    <div class="field">
+                        <label for="email">Email</label>
+                        <input
+                            id="email"
+                            v-model.trim="form.email"
+                            type="email"
+                            name="email"
+                            required
+                            autocomplete="email"
+                        />
+                        <span v-if="form.errors.email" class="error">{{
+                            form.errors.email
+                        }}</span>
+                    </div>
+
+                    <div class="field">
+                        <label for="password">Password</label>
+                        <input
+                            id="password"
+                            v-model="form.password"
+                            type="password"
+                            name="password"
+                            required
+                            autocomplete="new-password"
+                        />
+                        <span v-if="form.errors.password" class="error">{{
+                            form.errors.password
+                        }}</span>
+                    </div>
+
+                    <div class="field">
+                        <label for="password_confirmation">Confirm password</label>
+                        <input
+                            id="password_confirmation"
+                            v-model="form.password_confirmation"
+                            type="password"
+                            name="password_confirmation"
+                            required
+                            autocomplete="new-password"
+                        />
+                    </div>
+
+                    <AuthButton
+                        class="btn btn--primary"
+                        type="submit"
+                        label="Register"
+                        :disabled="form.processing"
                     />
-                    <span v-if="form.errors.name" class="error">{{
-                        form.errors.name
-                    }}</span>
+                </form>
+
+                <div class="row">
+                    <Link href="/login">Already registered?</Link>
                 </div>
-
-                <div class="field">
-                    <label for="email">Email</label>
-                    <input
-                        id="email"
-                        v-model="form.email"
-                        type="email"
-                        name="email"
-                        required
-                        autocomplete="email"
-                    />
-                    <span v-if="form.errors.email" class="error">{{
-                        form.errors.email
-                    }}</span>
-                </div>
-
-                <div class="field">
-                    <label for="password">Password</label>
-                    <input
-                        id="password"
-                        v-model="form.password"
-                        type="password"
-                        name="password"
-                        required
-                        autocomplete="new-password"
-                    />
-                    <span v-if="form.errors.password" class="error">{{
-                        form.errors.password
-                    }}</span>
-                </div>
-
-                <div class="field">
-                    <label for="password_confirmation">Confirm password</label>
-                    <input
-                        id="password_confirmation"
-                        v-model="form.password_confirmation"
-                        type="password"
-                        name="password_confirmation"
-                        required
-                        autocomplete="new-password"
-                    />
-                </div>
-
-                <button
-                    class="btn btn--primary"
-                    type="submit"
-                    :disabled="form.processing"
-                >
-                    Register
-                </button>
-            </form>
-
-            <div class="row">
-                <Link href="/login">Already registered?</Link>
             </div>
-        </div>
+        </BaseCard>
     </div>
 </template>
