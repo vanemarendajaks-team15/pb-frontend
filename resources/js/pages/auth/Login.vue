3da381c52076f842507148277462c50a20074886
Margus Käen <margus.kaen@gmail.com>
2026-04-27 20:53:01 +0300

    KAN-47 Quick frontend fix for pre-demo

--- resources/js/pages/auth/Login.vue
+++ resources/js/pages/auth/Login.vue
@@ -1,5 +1,8 @@
 <script setup>
 import { Head, Link, useForm } from '@inertiajs/vue3';
+import BaseCard from '../../components/ui/BaseCard.vue';
+import AuthButton from '@/components/buttons/AuthButton.vue';
+import TheHeader from '@/components/layout/TheHeader.vue';
 
 const props = defineProps({
     canResetPassword: {
@@ -34,71 +37,82 @@ function submit() {
 <template>
     <Head title="Log in" />
 
+    <TheHeader />
+
+
     <div class="page">
-        <div class="stack stack--lg">
-            <h1>Log in</h1>
+        <BaseCard>
+            <div class="stack stack--lg">
+                <h1>Log in</h1>
 
-            <div v-if="status" class="status" role="status">
-                {{ status }}
-            </div>
-
-            <form class="stack" @submit.prevent="submit">
-                <div class="field">
-                    <label for="email">Email</label>
-                    <input
-                        id="email"
-                        v-model="form.email"
-                        type="email"
-                        name="email"
-                        required
-                        autocomplete="username"
-                    />
-                    <span v-if="form.errors.email" class="error">{{
-                        form.errors.email
-                    }}</span>
+                <div v-if="status" class="status" role="status">
+                    {{ status }}
                 </div>
 
-                <div class="field">
-                    <label for="password">Password</label>
-                    <input
-                        id="password"
-                        v-model="form.password"
-                        type="password"
-                        name="password"
-                        required
-                        autocomplete="current-password"
+                <form class="stack" @submit.prevent="submit">
+                    <div class="field">
+                        <label for="email">Email</label>
+                        <input
+                            id="email"
+                            v-model="form.email"
+                            type="email"
+                            name="email"
+                            required
+                            autocomplete="username"
+                        />
+                        <span v-if="form.errors.email" class="error">{{
+                                form.errors.email
+                            }}</span>
+                    </div>
+
+                    <div class="field">
+                        <label for="password">Password</label>
+                        <input
+                            id="password"
+                            v-model="form.password"
+                            type="password"
+                            name="password"
+                            required
+                            autocomplete="current-password"
+                        />
+                        <span v-if="form.errors.password" class="error">{{
+                                form.errors.password
+                            }}</span>
+                    </div>
+
+                    <div class="field row">
+                        <input
+                            id="remember"
+                            v-model="form.remember"
+                            type="checkbox"
+                            name="remember"
+                        />
+                        <label for="remember">Remember me</label>
+                    </div>
+
+                    <AuthButton
+                        class="btn btn--primary"
+                        type="submit"
+                        :disabled="form.processing"
+                        label="Log in"
                     />
-                    <span v-if="form.errors.password" class="error">{{
-                        form.errors.password
-                    }}</span>
+                </form>
+
+                <div v-if="canResetPassword" class="row">
+                    <Link href="/forgot-password">Forgot password?</Link>
                 </div>
 
-                <div class="field row">
-                    <input
-                        id="remember"
-                        v-model="form.remember"
-                        type="checkbox"
-                        name="remember"
-                    />
-                    <label for="remember">Remember me</label>
+                <div v-if="canRegister" class="row">
+                    <Link href="/register">Create an account</Link>
                 </div>
-
-                <button
-                    class="btn btn--primary"
-                    type="submit"
-                    :disabled="form.processing"
-                >
-                    Log in
-                </button>
-            </form>
-
-            <div v-if="canResetPassword" class="row">
-                <Link href="/forgot-password">Forgot password?</Link>
             </div>
-
-            <div v-if="canRegister" class="row">
-                <Link href="/register">Create an account</Link>
-            </div>
-        </div>
+        </BaseCard>
     </div>
 </template>
+
+<style scoped>
+.page {
+    max-width: 400px;
+    margin: 0 auto;
+}
+</style>
